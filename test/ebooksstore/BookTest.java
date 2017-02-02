f5ccf6d72f11c696e198161cca0116cf9696a6d5
Lenovo <Lenovo@Lenovo-PC>
2017-02-02 21:34:31 +0200

    Homework: jUnit at Work

---
+++ test/ebooksstore/BookTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ebooksstore;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class BookTest {
+    
+    public BookTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of toString method, of class Book.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Book instance = new Book();
+        String expResult = "";
+        String result = instance.toString();
+        
+       
+        
+    }
+    
+}
